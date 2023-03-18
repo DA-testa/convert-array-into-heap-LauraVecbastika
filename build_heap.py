d6e4de3094fb70d01f9be93c4991b890d1c7570a
LauraVecbastika <124885748+LauraVecbastika@users.noreply.github.com>
2023-03-18 17:20:45 +0000

    test

--- build_heap.py
+++ build_heap.py
@@ -3,8 +3,8 @@ def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-    n = int((len(data)/2)-1):
-    for i in range (n, -1, -1)
+    n = len(data)
+    for i in range (n/2, -1, -1)
         min_heapify(data, n, i, swaps)
 
     return swaps
