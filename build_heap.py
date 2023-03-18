8e2eb049c18ae2364fb65fadf41cd32922e5a6fb
LauraVecbastika <124885748+LauraVecbastika@users.noreply.github.com>
2023-03-18 16:35:53 +0000

    test

--- build_heap.py
+++ build_heap.py
@@ -1,11 +1,28 @@
 # python3
-
-
+def min_heapify(data, size):
+    n=data.len()
+    leftChild=left(n)
+    rigthChild=right(n)
+    if leftChild< len(data) and data[leftChild]< data[n]:
+        smallest= leftChild
+    else:
+        smallest= n
+    if rigthChild <len(data) and data[rigthChild]<data[smallest]:
+        smallest=rigthChild
+    if smallest != n:
+        data[n], data[smallest]= data[smallest], data[n]
+    def left(n):
+        return 2*n+1
+    
+    def rigth(n):
+        return 2*n+2
 def build_heap(data):
     swaps = []
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
-
+    k= int((len(data)//2)-1):
+    for i in range (k, -1, -1)
+        min_heapify(data, n)
 
     return swaps
 
@@ -15,11 +32,20 @@ def main():
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
-
+    text=input()
+    text=text.upper()
 
     # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
+    
+    if text.startswith("I"):
+     n = int(input())
+        data = list(map(int, input().split()))
+    if text.startswith("F"):
+        file_name = input()
+        file_name = open('./test/'+file_name)
+        n = int(file_name.readline())
+        dataLasa = file_name.readline()
+        data =list(map(int, dataLasa.split()))))
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
